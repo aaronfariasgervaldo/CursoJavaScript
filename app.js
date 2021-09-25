61f5b64b407939d8b598e18a401246ef43dfa823
afarias <aaronfariasgervaldo@gmail.com>
2021-09-25 13:09:32 -0300

    Ejercicio Clase 1, curso JS Coderhouse

---
+++ app.js
@@ -0,0 +1,19 @@
+let nombreUss = prompt("Ingresa tu nombre");
+let apellidoUss = prompt("Ingresa tu apellido");
+let lenguajeUss = prompt("Cual es tu lenguaje favorito?");
+
+console.log("tu nombre es: " + nombreUss + apellidoUss + " y tu lenguaje favorito es: " + lenguajeUss);
+
+alert("Vamos a hacer algunas operaciones matematicas");
+
+let num1 = Number(prompt("Decime un número"));
+let num2 = Number(prompt("Decime otro número"));
+
+console.log("Si los sumamos, tu resultado es:");
+console.log(num1 + num2)
+console.log("Si los restamos, tu resultado es:");
+console.log(num1 - num2)
+console.log("Si los multiplicamos, tu resultado es:");
+console.log(num1 * num2)
+console.log("Si los dividimos, tu resultado es:");
+console.log(num1 / num2)
